a1161f49bb3a4b300008a5884484acd5f77aa9b3
Trinh Le Hai Nam <trinhlehainamjapan@gmail.com>
2020-08-14 18:18:48 +0900

    First Project
    Print Hello World

---
+++ Hello World/hello_world.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+void keep_window_open()
+{
+	char ch;
+	std::cin >> ch;
+}
+
+// The machine execute code here
+int main()
+{
+	// Print instruction text to screen monitor
+	std::cout << "Hello programming\n"; 
+	std::cout << "Here we go\n";
+	keep_window_open();
+}
+
+// token or double slashes : comment to explain things that you coded, helpful when someone doesn't write your
+// program but can understand when read these comment lines
+
+// << output operator, store information to cout to output to the screen monitor
+
+// C++ is a programming language to help human communicate with machine
+
+// compiler is a program to translate human's code to machine code
+
+// cout : character output stream
+
+// executable : is a machine's code translated from human's code
+
+// IDE : itergrated development environment
+
+// header : store definition of entity
+
+// linker : link multible objects (include your object and other object) together to execution file
+
+// main() : where machine run executation 
